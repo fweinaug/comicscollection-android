306258e9a0eb088bcc2975d1609d5cefa04e8847
Florian Weinaug <florianweinaug@outlook.de>
2018-05-02 18:35:03 +0200

    Added navigation drawer to main activity

--- app/src/main/java/de/florianweinaug/comicscollection/ui/main/MainActivity.kt
+++ app/src/main/java/de/florianweinaug/comicscollection/ui/main/MainActivity.kt
@@ -3,6 +3,9 @@ package de.florianweinaug.comicscollection.ui.main
 import android.arch.lifecycle.Observer
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.view.*
@@ -10,9 +13,10 @@ import de.florianweinaug.comicscollection.R
 import de.florianweinaug.comicscollection.model.Comic
 import de.florianweinaug.comicscollection.ui.settings.SettingsActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main_content.*
 import kotlinx.android.synthetic.main.comic_list.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var mViewModel: MainViewModel
     private lateinit var mAdapter: ComicListAdapter
@@ -24,6 +28,13 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
 
         if (comic_detail_container != null) {
             // The detail container view will be present only in the
@@ -51,6 +62,14 @@ class MainActivity : AppCompatActivity() {
                 .observe(this, Observer<List<Comic>> { if (it != null) mAdapter.update(it) })
     }
 
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_comic_list, menu)
         return true
@@ -58,11 +77,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem) =
             when (item.itemId) {
-                R.id.menu_settings -> {
-                    val intent = Intent(this, SettingsActivity::class.java)
-                    startActivity(intent)
-                    true
-                }
                 R.id.menu_add_comic -> {
                     val newFragment = AddComicDialog()
                     newFragment.show(supportFragmentManager, newFragment.tag)
@@ -71,4 +85,15 @@ class MainActivity : AppCompatActivity() {
                 else -> super.onOptionsItemSelected(item)
             }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_settings -> {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
 }
